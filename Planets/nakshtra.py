23588afe94a9afc0078c8c260838beb995e66f95
Tarun Brari <tarun.b@outlook.com>
2023-02-06 14:56:50 +0530

    Added Planets & Nakshatras
    Added two files.

---
+++ Planets/nakshtra.py
@@ -0,0 +1,5 @@
+# An array of nakshtras
+
+import array as arr
+
+planets = ['Surya', 'Chandra', 'Mangal', 'Guru', 'Shukra' , 'Shani', 'Rahu', 'Ketu']
---
+++ Planets/planets.py
@@ -0,0 +1,8 @@
+# An array of planets
+
+# Importing array for array creations
+import array as arr
+
+planets = ['Surya', 'Chandra', 'Mangal', 'Guru', 'Shukra' , 'Shani', 'Rahu', 'Ketu']
+print (planets)
+

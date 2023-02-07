6c412d911523067448d648fd61848c87e5f09788
Tarun Brari <tarun.b@outlook.com>
2023-02-07 14:43:36 +0530

    Removed the files
    Removed the planets files.

--- Planets/navgrah.py
+++
@@ -1,3 +0,0 @@
-# Defining the list of navgraha
-
-navgraha = ["Surya" , "Chandra" , "Mangal" , "Guru", "Budh" , "Shukra", "Shani", "Rahu" , "Ketu"]
--- Planets/rashi.py
+++
@@ -1,5 +0,0 @@
-# This file will deal with rashis
-
-rashi_naam = ["Mesh" , "Vrishabh" , "Mithun" , "Kark" , "Singh", "Kanya" ,
-         "Tula" , "Vrishchik" , "Dhanu" , "Makar" , "Kumbh", "Meen"]
-
